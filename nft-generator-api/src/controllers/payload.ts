f489565e7fd7bda8d6720fd01b18907ebfc58608
Kristjan Kosic - Chris <chris@ark.io>
2020-05-26 14:33:46 +0200

    chore: migrate generator helper api to monorepo

---
+++ nft-generator-api/src/controllers/payload.ts
@@ -0,0 +1,74 @@
+import { Controller } from "@arkecosystem/core-api";
+import { Container } from "@arkecosystem/core-kernel";
+import Hapi from "@hapi/hapi";
+
+import { buildResponse } from "../utils/client";
+import {
+    buildAcceptTrade,
+    buildAuction,
+    buildAuctionCancel,
+    buildBid,
+    buildBidCancel,
+} from "../utils/exchange-transactions";
+import { buildTokenAsset, buildTokenBurn, buildTokenCollection, buildTokenTransfer } from "../utils/transactions";
+
+@Container.injectable()
+export class PayloadController extends Controller {
+    public async hello(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        return { sparta: "moanlabe" };
+    }
+
+    public async generateTokenCollection(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildTokenCollection({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+
+    public async generateTokenAsset(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildTokenAsset({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+
+    public async generateTokenTransfer(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildTokenTransfer({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+
+    public async generateTokenBurn(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildTokenBurn({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+
+    public async generateTokenAuction(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildAuction({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+
+    public async generateTokenAuctionCancel(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildAuctionCancel({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+
+    public async generateTokenBid(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildBid({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+
+    public async generateTokenBidCancel(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildBidCancel({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+
+    public async generateTokenAcceptTrade(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+        const transaction = await buildAcceptTrade({ ...request.payload });
+
+        return buildResponse(transaction, request.query.apply);
+    }
+}
---
+++ nft-generator-api/src/utils/transactions.ts
@@ -0,0 +1,62 @@
+import { Interfaces } from "@packages/crypto";
+import { Builders } from "@protokol/nft-base-crypto";
+
+import { buildTransaction } from "./builder";
+
+export const buildTokenCollection = async (payloadData: {
+    name: string;
+    description: string;
+    maximumSupply: number;
+    jsonSchema: object;
+    nonce?: number;
+    passphrase: string;
+}): Promise<Interfaces.ITransactionData> => {
+    const nftRegisterCollectionBuilder = new Builders.NFTRegisterCollectionBuilder().NFTRegisterCollectionAsset({
+        name: payloadData.name,
+        description: payloadData.description,
+        maximumSupply: payloadData.maximumSupply,
+        jsonSchema: payloadData.jsonSchema,
+    });
+
+    return buildTransaction(nftRegisterCollectionBuilder, payloadData);
+};
+
+export const buildTokenAsset = async (payloadData: {
+    collectionId: string;
+    attributes: object;
+    nonce?: number;
+    passphrase: string;
+}): Promise<Interfaces.ITransactionData> => {
+    const nftCreateBuilder = new Builders.NFTCreateBuilder().NFTCreateToken({
+        collectionId: payloadData.collectionId,
+        attributes: payloadData.attributes,
+    });
+
+    return buildTransaction(nftCreateBuilder, payloadData);
+};
+
+export const buildTokenTransfer = async (payloadData: {
+    nftIds: string[];
+    recipientId: string;
+    nonce?: number;
+    passphrase: string;
+}): Promise<Interfaces.ITransactionData> => {
+    const nftTransferBuilder = new Builders.NFTTransferBuilder().NFTTransferAsset({
+        nftIds: payloadData.nftIds,
+        recipientId: payloadData.recipientId,
+    });
+
+    return buildTransaction(nftTransferBuilder, payloadData);
+};
+
+export const buildTokenBurn = async (payloadData: {
+    nftId: string;
+    nonce?: number;
+    passphrase: string;
+}): Promise<Interfaces.ITransactionData> => {
+    const nftBurnToken = new Builders.NFTBurnBuilder().NFTBurnAsset({
+        nftId: payloadData.nftId,
+    });
+
+    return buildTransaction(nftBurnToken, payloadData);
+};
