f41933a44dc84671691c7f8e18bc04d5ff227594
Kristjan Kosic - Chris <chris@ark.io>
2020-05-27 11:35:13 +0200

    remove clone script

--- scripts/clone.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/protokol/core-nft --recursive
-
-git submodule foreach -q --recursive \
-  'git switch \
-  $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
-
-
