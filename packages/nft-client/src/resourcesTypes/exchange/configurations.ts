90b007850d8739e4a6597246fbee49159651e983
KovacZan <zan@ark.io>
2020-06-30 09:35:09 +0200

    chore(nft-client): remove safety distance in interface

--- packages/nft-client/src/resourcesTypes/exchange/configurations.ts
+++ packages/nft-client/src/resourcesTypes/exchange/configurations.ts
@@ -14,8 +14,6 @@ export interface ExchangeConfigurations {
         };
     };
     transactions: {
-        defaults: {
-            safetyDistance: number;
-        };
+        defaults: {};
     };
 }
