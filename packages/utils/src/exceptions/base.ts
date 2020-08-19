851f0d64230a47c45c16149d03a9bf616f0d82a1
lubejmatej <54802172+lubejmatej@users.noreply.github.com>
2020-08-19 15:05:27 +0200

    feat: utils package (#131)
    * feat: add utils
    
    * chore: README description

---
+++ packages/utils/src/exceptions/base.ts
@@ -0,0 +1,28 @@
+/**
+ * @export
+ * @class Exception
+ * @extends {Error}
+ */
+export class Exception extends Error {
+    /**
+     * Creates an instance of Exception.
+     *
+     * @param {string} message
+     * @memberof Exception
+     */
+    public constructor(message: string) {
+        super(message);
+
+        Object.defineProperty(this, "message", {
+            enumerable: false,
+            value: message,
+        });
+
+        Object.defineProperty(this, "name", {
+            enumerable: false,
+            value: this.constructor.name,
+        });
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
