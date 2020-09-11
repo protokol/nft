6d65af1a2063fd5a8e45f472990c4c043625e388
KovacZan <zan@protokol.com>
2020-09-11 13:51:29 +0200

    fix(client): export guardian

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -3,6 +3,8 @@ export * as BaseResources from "./resources/nft/base";
 export * as ExchangeResources from "./resources/nft/exchange";
 export * as BaseResourcesTypes from "./resources-types/nft/base";
 export * as ExchangeResourcesTypes from "./resources-types/nft/exchange";
+export * as GuardianResources from "./resources/guardian";
+export * as GuardianResourcesTypes from "./resources-types/guardian";
 export { Timestamp } from "./resources-types/timestamp";
 export { PeerDiscovery } from "./peer-discovery";
 export { ConnectionManager } from "./connection-manager";
