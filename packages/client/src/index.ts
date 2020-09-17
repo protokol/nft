8db5caf3916266e8ad8b619725739cdbe2a16a5f
KovacZan <zan@protokol.com>
2020-09-17 10:36:09 +0200

    chore(client): export guardian connection

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -3,6 +3,7 @@ export * as BaseResources from "./resources/nft/base";
 export * as ExchangeResources from "./resources/nft/exchange";
 export * as BaseResourcesTypes from "./resources-types/nft/base";
 export * as ExchangeResourcesTypes from "./resources-types/nft/exchange";
+export * from "./guardian-connection";
 export * as GuardianResources from "./resources/guardian";
 export * as GuardianResourcesTypes from "./resources-types/guardian";
 export { Timestamp } from "./resources-types/timestamp";
