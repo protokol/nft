57df49fc361960a71878bbac5d45f02f12ebdc99
KovacZan <39158639+KovacZan@users.noreply.github.com>
2020-09-22 13:20:46 +0200

    test(client): peer discovery and guardian tests and improvments (#161)
    * test(client): peer discovery tests and improvments
    
    * feat(client): ApiResponseWithPagination
    
    * test(client): guardian tests
    
    * fix(client): remove dead code
    
    * fix(client): remove peerData object

---
+++ packages/client/__tests__/mocks/guardian/users.ts
@@ -0,0 +1,80 @@
+import nock from "nock";
+
+export const mockGuardianUsers = (host: string) => {
+    nock(host)
+        .get("/guardian/users")
+        .reply(200, {
+            meta: {
+                totalCountIsEstimate: false,
+                count: 1,
+                pageCount: 1,
+                totalCount: 1,
+                next: null,
+                previous: null,
+                self: "/guardian/users?page=1&limit=100",
+                first: "/guardian/users?page=1&limit=100",
+                last: "/guardian/users?page=1&limit=100",
+            },
+            data: [
+                {
+                    publicKey: "03287bfebba4c7881a0509717e71b34b63f31e40021c321f89ae04f84be6d6ac37",
+                    groups: ["group name"],
+                    permissions: [
+                        {
+                            kind: 1,
+                            types: [
+                                {
+                                    transactionType: 1,
+                                    transactionTypeGroup: 9002,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        });
+
+    nock(host)
+        .get("/guardian/users/03287bfebba4c7881a0509717e71b34b63f31e40021c321f89ae04f84be6d6ac37")
+        .reply(200, {
+            data: {
+                publicKey: "03287bfebba4c7881a0509717e71b34b63f31e40021c321f89ae04f84be6d6ac37",
+                groups: ["group name"],
+                permissions: [
+                    {
+                        kind: 1,
+                        types: [
+                            {
+                                transactionType: 1,
+                                transactionTypeGroup: 9002,
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
+
+    nock(host)
+        .get("/guardian/users/03287bfebba4c7881a0509717e71b34b63f31e40021c321f89ae04f84be6d6ac37/groups")
+        .reply(200, {
+            data: [
+                {
+                    name: "group name",
+                    priority: 1,
+                    active: false,
+                    default: false,
+                    permissions: [
+                        {
+                            kind: 1,
+                            types: [
+                                {
+                                    transactionType: 1,
+                                    transactionTypeGroup: 9002,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        });
+};
--- packages/client/src/resources/guardian/users.ts
+++ packages/client/src/resources/guardian/users.ts
@@ -1,17 +1,17 @@
-import { IResponse, Resource } from "@arkecosystem/client";
+import { ApiResponse, ApiResponseWithPagination, Resource } from "@arkecosystem/client";
 
 import { User } from "../../resources-types/guardian";
 
 export class Users extends Resource {
-    public async index(): Promise<IResponse<User>> {
+    public async index(): Promise<ApiResponseWithPagination<User>> {
         return this.sendGet("guardian/users");
     }
 
-    public async get(publicKey: string): Promise<IResponse<User>> {
+    public async get(publicKey: string): Promise<ApiResponse<User>> {
         return this.sendGet(`guardian/users/${publicKey}`);
     }
 
-    public async userGroups(publicKey: string): Promise<IResponse<User>> {
+    public async userGroups(publicKey: string): Promise<ApiResponse<User>> {
         return this.sendGet(`guardian/users/${publicKey}/groups`);
     }
 }
