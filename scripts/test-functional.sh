ae7dc55268fa0366169cc15d43a3a5ca20ba76b5
Kristjan Kosic <kristjan@protokol.com>
2020-10-06 15:37:48 +0200

    chore: remove scripts folder

--- scripts/test-functional.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd packages/nft-base-transactions && yarn test:functional:coverage && cd ../..
-cd packages/nft-exchange-transactions && yarn test:functional:coverage && cd ../..
-cd packages/guardian-transactions && yarn test:functional:coverage && cd ../..
--- scripts/test-integration.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd packages/nft-base-api && yarn test:integration:coverage && cd ../..
-cd packages/nft-exchange-api && yarn test:integration:coverage && cd ../..
-cd packages/guardian-api && yarn test:integration:coverage && cd ../..
