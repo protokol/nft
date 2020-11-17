51e13961ef7498392e6d455235fda1ff5c62967d
Kristjan Kosic <kristjan@protokol.com>
2020-11-17 08:57:25 +0100

    chore: script to run publish via pckg.json

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 cd packages
-cd nft-base-crypto && yarn npm publish --tag beta --access public --tolerate-republish && cd ..
-cd nft-base-transactions && yarn npm publish --tag beta --access public --tolerate-republish && cd ..
-cd nft-base-api && yarn npm publish --tag beta --access public --tolerate-republish && cd ..
-cd nft-exchange-crypto && yarn npm publish --tag beta --access public --tolerate-republish && cd ..
-cd nft-exchange-transactions && yarn npm publish --tag beta --access public --tolerate-republish && cd ..
-cd nft-exchange-api && yarn npm publish --tag beta --access public --tolerate-republish && cd ..
+cd nft-base-crypto && yarn publish:beta && cd ..
+cd nft-base-transactions && yarn publish:beta && cd ..
+cd nft-base-api && yarn publish:beta && cd ..
+cd nft-exchange-crypto && yarn publish:beta && cd ..
+cd nft-exchange-transactions && yarn publish:beta && cd ..
+cd nft-exchange-api && yarn publish:beta && cd ..
