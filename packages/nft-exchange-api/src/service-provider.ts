aecc28c22826daa8e2caa62076793e254401042e
Amadej Pevec <amadej.pevec@gmail.com>
2021-04-09 11:49:14 +0200

    feat: log plugin info on load

--- packages/nft-exchange-api/src/service-provider.ts
+++ packages/nft-exchange-api/src/service-provider.ts
@@ -1,10 +1,15 @@
 import { Identifiers, Server } from "@arkecosystem/core-api";
-import { Providers } from "@arkecosystem/core-kernel";
+import { Container, Contracts, Providers } from "@arkecosystem/core-kernel";
 
 import Handlers from "./handlers";
 
+const plugin = require("../package.json");
+
 export class ServiceProvider extends Providers.ServiceProvider {
 	public async register(): Promise<void> {
+		const logger: Contracts.Kernel.Logger = this.app.get(Container.Identifiers.LogService);
+		logger.info(`Loading plugin: ${plugin.name} with version ${plugin.version}.`);
+
 		for (const identifier of [Identifiers.HTTP, Identifiers.HTTPS]) {
 			if (this.app.isBound<Server>(identifier)) {
 				await this.app.get<Server>(identifier).register({
--- packages/nft-exchange-transactions/src/service-provider.ts
+++ packages/nft-exchange-transactions/src/service-provider.ts
@@ -9,8 +9,13 @@ import {
 } from "./handlers";
 import { auctionIndexer, bidIndexer, NFTExchangeIndexers } from "./wallet-indexes";
 
+const plugin = require("../package.json");
+
 export class ServiceProvider extends Providers.ServiceProvider {
     public async register(): Promise<void> {
+        const logger: Contracts.Kernel.Logger = this.app.get(Container.Identifiers.LogService);
+        logger.info(`Loading plugin: ${plugin.name} with version ${plugin.version}.`);
+
         this.registerIndexers();
 
         this.app.bind(Container.Identifiers.TransactionHandler).to(NFTAuctionHandler);
